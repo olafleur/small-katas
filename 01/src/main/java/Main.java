005710b95e87b6f9dae7b13ffa34c5ebdd9a14de
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-02-17 10:44:21 -0500

    Add Humanoids

--- 01/src/main/java/Main.java
+++ 01/src/main/java/Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
+        Humanoid humanoid1 = new Humanoid();
+        humanoid1.setHumanoidType("Lady with high heels");
+        humanoid1.walk();
 
+        Humanoid humanoid2 = new Humanoid();
+        humanoid2.setHumanoidType("Ninja");
+        humanoid2.walk();
+
+        Humanoid humanoid3 = new Humanoid();
+        humanoid3.setHumanoidType("Pirate");
+        humanoid3.walk();
+
+        Humanoid humanoid4 = new Humanoid();
+        humanoid4.setHumanoidType("Robot");
+        humanoid4.walk();
+
+        Humanoid humanoid5 = new Humanoid();
+        humanoid5.setHumanoidType("Regular human");
+        humanoid5.walk();
     }
 }
