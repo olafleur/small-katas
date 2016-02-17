3be75e711e271d739eda5499022e67c64aceac28
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-02-17 10:41:17 -0500

    Add JUnit and AssertJ

--- 01/src/test/java/HumanoidTest.java
+++ 01/src/test/java/HumanoidTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HumanoidTest {
+    @Test
+    public void stupidTest() {
+        assertThat(true).isFalse();
+    }
 }
