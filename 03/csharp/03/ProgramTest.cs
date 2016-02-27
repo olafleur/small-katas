66bdf8c04cd02c6a09c77650cf7e4267a9bbc9c1
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-02-27 13:25:23 -0500

    03 csharp + test

---
+++ 03/csharp/03/ProgramTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+
+namespace Application
+{
+	[TestFixture]
+	public class ProgramTest
+	{
+		private class PrintInterceptor : StringWriter {
+			public List<String> getLines()
+			{
+				var sb = GetStringBuilder ();
+				var result = sb.ToString ();
+
+				var splitted = result.Split ('\n');
+
+				return splitted.ToList ();
+			}
+		}	
+
+		[Test]
+		public void BasicTest ()
+		{
+			// given
+			var printInterceptor = new PrintInterceptor();
+			Console.SetOut(printInterceptor);
+
+			// when
+			MainClass.Main(null);
+
+			// then
+			var outputLines = printInterceptor.getLines();
+
+			outputLines.ElementAt (0).Should ().Be ("rick : 50");
+			outputLines.ElementAt (1).Should ().Be ("josh : 5");
+		}
+	}
+}
+
