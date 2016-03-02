c0ab4216771ed05e9a4c594e40e4ac96e0297a7d
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-03-02 15:57:45 -0500

    Finish Python 02

--- 02/python/stats-calculator.py
+++ 02/python/stats-calculator.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 import csv
 
@@ -40,8 +41,43 @@ class StatsCalculator:
         return teams
 
 
+    @staticmethod
+    def get_teams_over_points_average():
+        csv_file = os.getcwd() + "/input.csv"
+        counter = 0
+        somme = 0
+        teams = []
+
+        with open('input.csv', 'rb') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for truc in spamreader:
+                if counter > 0:
+                    points = int(truc[7])
+
+                    somme += points
+                counter = counter+1
+
+        # Lire l'autre fichier
+        moyenne = float(somme) / counter
+        counter = 0
+
+        with open('input.csv', 'rb') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for truc in spamreader:
+                if counter > 0:
+                    points = int(truc[7])
+
+                    if points > moyenne:
+                        teams.append(truc[1])
+                counter = counter+1
+
+        return teams
 
 
-
-
-print(StatsCalculator.get_teams_under_age_average())
+print("Les équipes dont l'âge moyen est en dessous de la moyenne sont :")
+for team in StatsCalculator.get_teams_under_age_average():
+    print team
+print ""
+print("Les équipes dont le nombre de points moyen est au-dessus de la moyenne sont :")
+for team in StatsCalculator.get_teams_over_points_average():
+    print team
