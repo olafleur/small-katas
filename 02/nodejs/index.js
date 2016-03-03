c36fb7374f5f505852a157d5978732345c9c4e65
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-03-03 17:13:54 -0500

    02 wip

--- 02/nodejs/index.js
+++ 02/nodejs/index.js
@@ -1,6 +1,10 @@
 fs = require('fs');
 readline = require('readline');
 
+var counter = 0;
+var somme = 0;
+var teams = [];
+
 function StatsCalculator() {
 }
 
@@ -10,7 +14,47 @@ StatsCalculator.getTeamsUnderAgeAverage = function () {
     });
 
     lineReader.on('line', function (line) {
-        console.log(line);
+        if(counter > 0) {
+            var truc = line.split(",");
+
+            var age = parseFloat(truc[2]);
+
+            somme += age;
+        }
+
+        counter++;
+    });
+
+    lineReader.on('close', function () {
+        var moyenne = somme / counter;
+        counter = 0;
+
+        var lineReader2 = readline.createInterface({
+            input: fs.createReadStream('input.csv')
+        });
+
+        lineReader2.on('line', function (line) {
+            if(counter > 0) {
+                var truc = line.split(",");
+
+                var age = parseFloat(truc[2]);
+
+                if(age >= moyenne) {
+                    /* do nothing */
+                } else {
+                    teams.push(truc[1]);
+                }
+            }
+
+            counter++;
+        });
+
+        lineReader2.on('close', function() {
+            console.log("Les équipes dont l'âge moyen est en dessous de la moyenne sont :");
+            for(var i = 0; i < teams.length; i++) {
+                console.log(teams[i]);
+            }
+        });
     });
 };
 
